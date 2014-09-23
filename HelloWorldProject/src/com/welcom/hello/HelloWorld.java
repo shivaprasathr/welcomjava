65a7b41eeadef68b7be3daf147081661a7807794
291096 <291096@PC285009.cts.com>
2014-09-23 15:21:09 +0530

    initial checkin

---
+++ HelloWorldProject/src/com/welcom/hello/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.welcom.hello;
+
+/**
+ * @author 291096
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+
+	}
+
+}
